556a9ded22d03bb419ee7ccd5f27f184f121342a
Naoki Maruyama <m.naoki85@gmail.com>
2022-03-23 23:25:59 +0900

    update : constractor added. to  claaLesson4/Cleric.java

--- classLesson4/Cleric.java
+++ classLesson4/Cleric.java
@@ -3,9 +3,29 @@ import java.util.*;
 public class Cleric{
     String name;
     int hp = 50;
-    final int MAXHP = 50;
+    static int MAXHP = 50;
     int mp = 10;
-    final int MAXMP = 10;
+    static int MAXMP = 10;
+
+    Cleric(String name, int hp, int mp){
+        this.name = name;
+        this.hp = hp;
+        this.mp = mp;
+    }
+
+    Cleric(String name, int hp){
+        // this.name = name;
+        // this.hp = hp;
+        // this.mp = Cleric.MAXMP;
+        this(name, hp, Cleric.MAXMP)
+    }
+
+    Cleric(String name){
+        // this.name = name;
+        // this.hp = Cleric.MAXHP;
+        // this.mp = Cleric.MAXMP;
+        this(name, Cleric.MAXHP, Cleric.MAXMP);
+    }
 
     void selfAid(){
         this.mp -= 5;
